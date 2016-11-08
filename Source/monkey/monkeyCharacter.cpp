cdbe292d9df653f1af82cc721895e0c52929be38
thePLO ENTI <pdemiguel@enti.cat>
2016-11-08 19:28:10 +0100

    update

--- Source/monkey/monkeyCharacter.cpp
+++ Source/monkey/monkeyCharacter.cpp
@@ -60,14 +60,14 @@ void AmonkeyCharacter::CTick(float deltaTime) {
 			GetCharacterMovement()->JumpZVelocity = 1200.f;
 			GetCharacterMovement()->AirControl = 0.8f;
 			GetCharacterMovement()->MaxAcceleration = 2048.0f;
-
 		}
 		else {
 			GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
 			GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
-			GetCharacterMovement()->JumpZVelocity = 600.f;
+			GetCharacterMovement()->JumpZVelocity = 50.f;
 			GetCharacterMovement()->AirControl = 2.0f;
 			GetCharacterMovement()->MaxAcceleration = 1600.0f;
+			GetCharacterMovement()->AddImpulse(FVector(0.0f, 0.0f, 1700.0f*deltaTime));
 		}
 	}
 	else {
